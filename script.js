7944e1fea962c70d9d82f3a8dab7f2b15a0b03ff
Camila <camila.rodriguez.aguila@gmail.com>
2024-02-16 09:02:56 -0700

    check mark correction

--- script.js
+++ script.js
@@ -5,40 +5,6 @@ function addTask() {
     if (inputBox.value === "") {
         alert("You must write something!");
     } else {
-        let li = document.createElement("li"); // Fixed typo here
-        li.innerHTML = inputBox.value;
-        listContainer.appendChild(li);
-        let span = document.createElement("span");
-        span.innerHTML = "\u00d7";
-        li.appendChild(span);
-    }""
-    inputBox.value = "";
-    saveData();
-}
-listContainer.addEventListener("click", function(e){
-    if(e.target.tagName === "LI"){
-        e.target.classList.toggle("checked");
-        saveData();
-    }
-    else if(e.target.tagName === "SPAN"){
-        e.target.parentElement.remove();
-        saveData();
-    }
-}, false);
-
-function saveData(){
-    localStorage.setItem("data", listContainer.innerHTML);
-}
-
-function showTask(){
-    listContainer.innerHTML = localStorage.getItem("data");
-}
-showTask()
-
-function addTask() {
-    if (inputBox.value === "") {
-        alert("You must write something!");
-    } else {
         let li = document.createElement("li");
         li.innerHTML = inputBox.value;
         listContainer.appendChild(li);
