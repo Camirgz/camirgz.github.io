104a61fce702507b01924fb04f7da208385de6c3
Camila <camila.rodriguez.aguila@gmail.com>
2024-02-16 08:59:22 -0700

    confirmación de eliminación

--- script.js
+++ script.js
@@ -34,3 +34,40 @@ function showTask(){
     listContainer.innerHTML = localStorage.getItem("data");
 }
 showTask()
+
+function addTask() {
+    if (inputBox.value === "") {
+        alert("You must write something!");
+    } else {
+        let li = document.createElement("li");
+        li.innerHTML = inputBox.value;
+        listContainer.appendChild(li);
+        let span = document.createElement("span");
+        span.innerHTML = "\u00d7";
+        li.appendChild(span);
+    }
+    inputBox.value = "";
+    saveData();
+}
+
+listContainer.addEventListener("click", function(e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        saveData();
+    } else if (e.target.tagName === "SPAN") {
+        let confirmation = confirm("Are you sure you want to delete this task?");
+        if (confirmation) {
+            e.target.parentElement.remove();
+            saveData();
+        }
+    }
+}, false);
+
+function saveData() {
+    localStorage.setItem("data", listContainer.innerHTML);
+}
+
+function showTask() {
+    listContainer.innerHTML = localStorage.getItem("data");
+}
+showTask();
